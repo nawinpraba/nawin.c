54d636a664ad4fc56f8902a5c79b1db822142c2f
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-01-31 15:09:48 +0530

    Create ns.c

---
+++ ns.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h.
+void main()
+{
+int num, i,sum=0;
+printf(" enter the number");
+scanf("%d",&num);
+for(i=1,i<=1,++i)
+{
+sum+=i;
+}
+{
+printf("sum=%d",sum);
+}
+return 0;
+}
