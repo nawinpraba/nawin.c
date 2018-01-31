3d124b753c5773f0cfc10922f089e79b2eec23d5
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-01-31 14:51:02 +0530

    Create ovn.c

---
+++ ovn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int char;
+printf("enter the character");
+scanf("%c',&char);
+if((char>='a' && char<=='z')
+{
+printf("%c charcter is alphabet",char);
+}
+else
+{
+printf"%c no ",char);
+}
+getch();
+}
