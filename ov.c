a8e2b52fc50087383b217ee49225d7ed96ff1f06
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-01-31 14:35:36 +0530

    Create ov.c

---
+++ ov.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter the character n");
+scanf("%c",&n);
+if(n==a,n==e,n==i,n==o,i==u)
+{
+printf("%c is a vowel ",n);
+}
+else
+{
+ptintf("%c is conosant",n);
+}
+getch();
+}
+
